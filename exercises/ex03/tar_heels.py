ab597cb69a340a00733978439d1874f4748f9e72
Robert Cole Hepler <coleh1@ad.unc.edu>
2021-09-16 19:33:51 -0400

    exo3

--- exercises/ex03/tar_heels.py
+++ exercises/ex03/tar_heels.py
@@ -1,6 +1,17 @@
 """An exercise in remainders and boolean logic."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730400371"
 
+number: str = input("Enter an int: ")
+number_int = int(number)
 
-# Begin your solution here...
+if number_int % 14 == 0:
+    print("TAR HEELS")
+else:
+    if number_int % 2 == 0:
+        print("TAR")
+    else:
+        if number_int % 7 == 0:
+            print("HEELS")
+        else:
+            print("CAROLINA")
